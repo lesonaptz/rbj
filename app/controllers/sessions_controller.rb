b632e8766f6c83a7136d035b479d1a9ef1c8b15d
lesonapt <lesonapt@gmail.com>
2013-09-07 16:11:52 +0700

    session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+   
     data = request.env['omniauth.auth'].extra.raw_info
-
     # render :text => data.inspect
-    if user = User.where(:email => data.email || data.emailAddress || ['twitter',data.id,'@gmail.com'].join('') ).first
-      @newuser = user     
+    if @user = User.where(:email => data.email || data.emailAddress || ['twitter',data.id,'@gmail.com'].join('') ).first
+      @user
     else 
-      @newuser = User.create!(:email => data.email || data.emailAddress || ['twitter',data.id,'@gmail.com'].join(''), :password => Devise.friendly_token[0,20]) 
+      @user = User.create!(:email => data.email || data.emailAddress || ['twitter',data.id,'@gmail.com'].join(''), :password => Devise.friendly_token[0,20]) 
     end
 
-    if @newuser.persisted?      
+    if @user.persisted?      
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @newuser, :event => :authentication
+      sign_in_and_redirect @user, :event => :authentication
     else      
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
