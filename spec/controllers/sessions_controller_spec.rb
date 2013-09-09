dc0d31eaca3125d0ded458a4d4a51ffc16392053
lesonapt <lesonapt@gmail.com>
2013-09-09 15:34:39 +0700

    rspect

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,16 +1,64 @@
 require 'spec_helper'
 
+
 describe SessionsController, "OmniAuth" do
-	
+  include Devise::TestHelpers	
   before do
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-    request.env["devise.mapping"] = Devise.mappings[:user]    
+    request.env["devise.mapping"] = Devise.mappings[:user] 
   end
 
-  it "sets a session variable to the OmniAuth auth hash" do
-  	# get 'create' , :workflow_state => 'active'
-    # puts assigns[:newuser].to_yaml
-    request.env["omniauth.auth"]['uid'].should == '1234'
+
+  it "login whith twitter" do
+    request.env["omniauth.auth"] = stub_env_for_omniauth_twitter
+    get 'create' 
+    assigns[:user].should be_true    
   end
+
+  it "login with linkedin" do
+    request.env["omniauth.auth"] = stub_env_for_omniauth_linkedin    
+    get 'create' 
+     assigns[:user].should be_true   
+  end
+
+  it "login with facebook" do
+    request.env["omniauth.auth"] = stub_env_for_omniauth_facebook    
+    get 'create' 
+    assigns[:user].should be_true   
+  end
+
+
+  def stub_env_for_omniauth_facebook
+    omniauth_hash = {
+     :extra => {
+      :provider => "facebook",      
+      :raw_info   => {:name   => "Le Anh Son", :email => "lesonapt@gmail.com", :id => "1234"},
+      :credentials => {:token => "testtoken234tsdf"}
+      }
+    }
+    OmniAuth.config.add_mock(:facebook, omniauth_hash)
+  end
+
+  def stub_env_for_omniauth_twitter
+    omniauth_hash = {
+     :extra => {
+      :provider => "twitter",      
+      :raw_info   => {:name   => "Le Anh Son", :id => "1731160094", },
+      :credentials => {:token => "testtoken234tsdf"}
+      }
+    }
+    OmniAuth.config.add_mock(:twitter, omniauth_hash)
+  end
+
+  def stub_env_for_omniauth_linkedin
+     omniauth_hash = {
+     :extra => {
+      :provider => "linkedin",      
+      :raw_info   => {:name   => "Le Anh Son",:id => "1234"},
+      :credentials => {:token => "testtoken234tsdf"}
+      }
+    }    
+    OmniAuth.config.add_mock(:linkedin, omniauth_hash)
+  end
+
 
 end
