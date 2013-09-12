81c133136362de58b901883667df0a047a3b34b5
lesonapt <lesonapt@gmail.com>
2013-09-12 08:18:01 +0700

    repair contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ActiveRecord::Base
-	validates_format_of :email, :with => /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/
+	# validates_format_of :email, :with => /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/, :message => 'email not fomat'
 	validates_numericality_of :phone, :only_integer => true, :message => "can only be whole number."
 end
