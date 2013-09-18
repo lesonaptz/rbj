a3a00d86da0f1d7690508fcd768e6ce2fdc2bbd5
lesonapt <lesonapt@gmail.com>
2013-09-18 07:47:13 +0700

    get object

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,13 +2,26 @@ class Contact < ActiveRecord::Base
 	# validates_format_of :email, :with => /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/, :message => 'email not fomat'
 	validates_numericality_of :phone, :only_integer => true, :message => "can only be whole number."
 
+	def oktest
+	end
+
 	def self.get_contact_api(api_token, type = 'facebook')
 		contact_data = Array.new
-
 		if type == 'facebook'
-			graph = Koala::Facebook::API.new(api_token)
-	    user = graph.get_object("me")
-	    contact_data = graph.fql_query("select email, contact_email, name, uid from user where uid in (select uid2 from friend where uid1 = #{user["id"]} )")
+			# api_token = '346465042117861|NCRsUCHyvkf4OqbN4IGce9Cb4j0'
+			# graph = Koala::Facebook::API.new(api_token)
+	    # user = graph.get_object("me")
+	  	# las = graph.fql_query("select email, contact_email, name, uid from user where uid in (select uid2 from friend where uid1 = #{user["id"]} )")
+					
+			
+			@graph = Koala::Facebook::GraphAPI.new(api_token)
+			data =  @graph.get_connections("me", "friends")
+			data.each do |f|
+				friend = @graph.get_object(f['id'])
+				contact_data << friend
+			end
+			contact_data = data 
+
 		elsif type == 'twitter'
 			# Twitter.update("I'm demo gemfile to post to wall")
 		  # contact_data = Twitter.user(258286822)	
@@ -30,7 +43,8 @@ class Contact < ActiveRecord::Base
 			
 			# get connections error
 			contact_data = client.connections
-		end					
+		end		
+		contact_data 		
 		self.contact_add(self.filter_contact(contact_data, type))
 	end
 
